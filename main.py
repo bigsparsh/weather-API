b25f719cc8e1176ad2c8c2ef5827d49a1b67edfd
bigsparsh <sparshsingh7586@gmail.com>
2023-06-18 19:02:55 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/api/v1/<station>/<date>")
+def find(station, date):
+    df = pd.read_csv(f'data-small/TG_STAID{str(station).zfill(6)}.txt',
+                     parse_dates=['    DATE'],
+                     skiprows=20)
+    df['   TG'] = df['   TG'] / 10
+    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    temperature = df['TG'].loc[df['    DATE'] == date].squeeze()
+    return {"Date": date,
+            "Station": station,
+            "Temperature": str(temperature)}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
