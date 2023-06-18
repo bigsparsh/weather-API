c5f037b1128fca6ec27d3cfd144a2535f25e755f
bigsparsh <sparshsingh7586@gmail.com>
2023-06-18 20:54:22 +0530

    Added more link options like annual and specific station

--- main.py
+++ main.py
@@ -19,12 +19,36 @@ def find(station, date):
                      parse_dates=['    DATE'],
                      skiprows=20)
     df['   TG'] = df['   TG'] / 10
-    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    df['TG'] = df['   TG'].mask(df['   TG'] == -999.9, np.nan)
     temperature = df['TG'].loc[df['    DATE'] == date].squeeze()
     return {"Date": date,
             "Station": station,
             "Temperature": str(temperature)}
 
 
+@app.route("/api/v1/<station>")
+def station_info(station):
+    df = pd.read_csv(f'data-small/TG_STAID{str(station).zfill(6)}.txt',
+                     parse_dates=['    DATE'],
+                     skiprows=20)
+    df['   TG'] = df['   TG'] / 10
+    df['TG'] = df['   TG'].mask(df['   TG'] == -999.9, np.nan)
+    return df.to_dict(orient='records')
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def annual(station, year):
+    df = pd.read_csv(f'data-small/TG_STAID{str(station).zfill(6)}.txt',
+                     parse_dates=['    DATE'],
+                     skiprows=20)
+    df['   TG'] = df['   TG'] / 10
+    df['TG'] = df['   TG'].mask(df['   TG'] == -999.9, np.nan)
+    df['    DATE'] = df['    DATE'].astype(str)
+    df = df.loc[df['    DATE'].str.startswith(str(year))]
+    return df.to_dict(orient='records')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# df = df.loc[str(df["    DATE"]).startswith('1860')]
